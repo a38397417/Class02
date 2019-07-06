b732acc6d8ccab7531dda1b061542f89edcf5475
njcit-02 <971632489@qq.com>
2019-07-06 17:37:44 +0800

    inital project

---
+++ Class02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class02
+//
+//  Created by njcit-02 on 2019/7/6.
+//  Copyright © 2019 耿小凡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

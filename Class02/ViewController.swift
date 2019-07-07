1e55427d0c203faaff2c89b8ed5d97d2703125f3
njcit-02 <971632489@qq.com>
2019-07-07 17:11:06 +0800

    0707

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var result : float_t = 10.2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //
+        
+        let 如果我去超市 = true
+        //有橘子
+         let 如果看到橘子 = true
+        
+        if 如果我去超市{
+            var 卖西瓜的数量 = 1
+            
+            if 如果看到橘子{
+            卖西瓜的数量 = 10
+        }
+        print("我要卖：\( 卖西瓜的数量 )颗西瓜")
+
+            
+        
+    
+    }
+
+    func add(a:Int,b:Int) -> Int {
+        return a+b
     }
 
 
 }
-
+}

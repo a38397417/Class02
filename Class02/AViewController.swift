959514e9fe882c38563e62e6c87bae46b40b37bd
njcit-02 <971632489@qq.com>
2019-07-09 17:01:37 +0800

    0709

--- Class02/AViewController.swift
+++ Class02/AViewController.swift
@@ -10,17 +10,47 @@ import UIKit
 
 class AViewController: UIViewController {
 @IBOutlet weak var lbMSG: UITextField!
+    
+
+
+    @IBOutlet weak var btnHiBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification , object:nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillHideNotification , object:nil)
     }
     @IBAction func btnTestClick(_ sender: UIButton) {
         print("我干了啥")
         lbMSG.text = "我要铜锣烧"
         lbMSG.textColor = UIColor.red
+        btnHiBottomConstraint.constant =  400
     }
 
+    
+    //Stac
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            _ = keyboardRectangle.height
+        }
+    }
+    
+    //tencent.com
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let keyboardHeight = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+        print("猜猜看：\(keyboardHeight)")
+         btnHiBottomConstraint.constant =  keyboardHeight
+    }
+    
+//    @objc func keyboardWillHide(notification: NSNotification) {
+//        let keyboardHeight = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+//        print(keyboardHeight)
+//    }
     /*
     // MARK: - Navigation
 
